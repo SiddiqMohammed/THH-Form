8bd618c349e9dfcfce70bfef3ae3bfa27ed60fda
Clyde <clydeibalez@gmail.com>
2020-10-14 15:43:15 +0400

    changed the font

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -11,6 +11,8 @@ function Contacts() {
     const [emirates, setEmirates] = useState("");
     const [isMale, setIsMale] = useState("")
     const [isFemale, setIsFemale] = useState("")
+    const [isInterested, setIsInterested] = useState("")
+    const [isNot, setIsNot] = useState("")
     // const [message, setMessage] = useState("");
 
     const [loader, setLoader] = useState(false)
@@ -28,6 +30,8 @@ function Contacts() {
             emirates: emirates,
             isMale: isMale,
             isFemale: isFemale,
+            isInterested: isInterested,
+            isNot: isNot,
             // message: message
         })
         .then(() => {
@@ -48,6 +52,8 @@ function Contacts() {
         setEmirates("");
         setIsMale("");
         setIsFemale("");
+        setIsInterested("");
+        setIsNot("");
         // setMessage("");
     }
 
@@ -62,24 +68,6 @@ function Contacts() {
             value={name}
             onChange={(e) => setName(e.target.value)}
             />
-
-            <label>Gender</label>
-            <label>
-            <input 
-            type="radio"
-            value="Male"
-            style={{ marginRight: '10px' }}
-            checked={isMale}
-            onChange={(e) => {setIsMale(e.target.value); setIsFemale("")}} />
-            Male</label>
-            <label>
-            <input 
-            type="radio"
-            value="Female"
-            style={{ marginBottom: '30px', marginRight: '10px' }}
-            checked={isFemale}
-            onChange={(e) => {setIsFemale(e.target.value); setIsMale("")}} />
-            Female</label>
 
             {/* <label>Gender</label>
             <input 
@@ -120,14 +108,50 @@ function Contacts() {
             required
             value={emirates} 
             onChange={(e) => setEmirates(e.target.value)}>
-                <option value="apple">Abu Dhabi</option>
-                <option value="orange">Ajman</option>
-                <option value="dubai">Dubai</option>
-                <option value="fujairah">Fujairah</option>
-                <option value="rasalkhaimah">Ras Al-Khaimah</option>
-                <option value="sharjah">Sharjah</option>
-                <option value="ummalquwain">Umm Al-Quwain</option>
+                <option value="Abu Dhabi">Abu Dhabi</option>
+                <option value="Ajman">Ajman</option>
+                <option value="Dubai">Dubai</option>
+                <option value="Fujairah">Fujairah</option>
+                <option value="Ras Alkhaimah">Ras Al-Khaimah</option>
+                <option value="Sharjah">Sharjah</option>
+                <option value="Umm Al Quwain">Umm Al-Quwain</option>
             </select>
+
+            <label>Gender</label>
+            <label>
+            <input 
+            type="radio"
+            value="Male"
+            style={{ marginRight: '10px' }}
+            checked={isMale}
+            onChange={(e) => {setIsMale(e.target.value); setIsFemale("")}} />
+            Male</label>
+            <label>
+            <input 
+            type="radio"
+            value="Female"
+            style={{ marginBottom: '30px', marginRight: '10px' }}
+            checked={isFemale}
+            onChange={(e) => {setIsFemale(e.target.value); setIsMale("")}} />
+            Female</label>
+
+            <label>Are you interested in Part-Time Promoter and Event Roles?</label>
+            <label>
+            <input 
+            type="radio"
+            value="Yes"
+            style={{ marginRight: '10px' }}
+            checked={isInterested}
+            onChange={(e) => {setIsInterested(e.target.value); setIsNot("")}} />
+            Yes, I'm Interested</label>
+            <label>
+            <input 
+            type="radio"
+            value="No"
+            style={{ marginBottom: '30px', marginRight: '10px' }}
+            checked={isNot}
+            onChange={(e) => {setIsNot(e.target.value); setIsInterested("")}} />
+            No</label>
 
             {/* <label>Message</label>
             <textarea 
